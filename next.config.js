8a2855ac38a220a6b459dfcfd9350aa5cf7fab4b
Your Name <you@example.com>
2025-06-26 15:22:02 +0900

    0626-7

--- next.config.js
+++ next.config.js
@@ -1,1 +1,7 @@
-// Placeholder for next.config.js
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // 또는 'standalone' 가능
+};
+
+module.exports = nextConfig;
